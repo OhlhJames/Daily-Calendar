9ec654fb1f49f4e17cf1adaaac61189cf7545fd1
IsItIzzet <ohlhjames@gmail.com>
2024-01-30 11:30:41 -0600

    added functionality so that the local storage could be read and added to list after refreshing so that the list stays up after page is reloaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,18 +7,21 @@ $(function () {
     var currentHour = today.format('H');
     $('#currentDay').text(today.format('MMM D, YYYY'));
     $('.btn').on('click',function() {
-        var scheduledEvent = $(this).siblings('.description').val()
-        var eventTime = $(this).parent().attr('id')
-        localStorage.setItem(eventTime, scheduledEvent) 
+        var newEvent = $(this).siblings('.description').val()
+        var newEventTime = $(this).parent().attr('id')
+        localStorage.setItem(newEventTime, newEvent) 
     })
     for(i=0;i<=23;i++){
         var listedTime = $('.root').children().eq(i).attr('id');
         if(currentHour > listedTime){
             $('.root').children().eq(i).attr('class', 'row time-block past')
+            $('.root').children().eq(i).attr('time', 'past')
         }else if(listedTime === currentHour){
             $('.root').children().eq(i).attr('class', 'row time-block present')
+            $('.root').children().eq(i).attr('time', 'present')
         }else if(currentHour < listedTime){
             $('.root').children().eq(i).attr('class', 'row time-block future')
+            $('.root').children().eq(i).attr('time', 'future')
         }
     }
     // TODO: Add code to apply the past, present, or future class to each time
@@ -26,7 +29,15 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    for(i = 9;i <= 17 ;i++){ 
+        var scheduledEvent = localStorage.getItem(i);
+        if($('.root').children().eq(i).prop('past')){
+            $('.root').children().eq((i-9)).children().eq(1).val('Event has passed')
+        }else{
+            $('.root').children().eq((i-9)).children().eq(1).val(scheduledEvent)
+        }
+            
+    }
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
