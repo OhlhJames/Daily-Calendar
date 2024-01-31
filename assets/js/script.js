034e11f569577d05e96ed4b3f20b69f0493df622
IsItIzzet <ohlhjames@gmail.com>
2024-01-31 12:17:16 -0600

    added comments throughout code explaining how different pieces work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+/* Wraps all code that interacts with the DOM in a call to jQuery to ensure that
+the code isn't run until the browser has finished rendering all the elements
+ in the html. */
 $(function () {
     //sets variable for current day and sets that day to be the header of the page
     var today = dayjs();
-    var currentHour = today.format('H');
     $('#currentDay').text(today.format('MMM D, YYYY'));
+    //Adds functioanlity to the save button so that events can be added to the calender
     $('.btn').on('click',function() {
         var newEvent = $(this).siblings('.description').val()
         var newEventTime = $(this).parent().attr('id')
         localStorage.setItem(newEventTime, newEvent) 
     })
+    //iterates over the calender to check if each time is past present or future and sets an appropriate class so that css can style it accordingly
     for(i=0;i<=8;i++){
         var listedTime = $('.root').children().eq(i).attr('id');
+        var currentHour = today.format('H');
         listedTime = parseInt(listedTime)
         currentHour = parseInt(currentHour)
         if(currentHour < listedTime){
@@ -28,27 +30,20 @@ $(function () {
             $('.root').children().eq(i).attr('data-time', 'past')
         }
     }
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
+    //iterates over each hour to check for saved events and then populates those events
+    //also changes events that happened in the past to "This event has passed"
     for(i = 0;i <= 8 ;i++){ 
         var timeJump = (i+9)
         var scheduledEvent = localStorage.getItem(timeJump);
         var element = $('.root').children().eq(i);
         var timeChecker = element.data('time') 
-        
         if( timeChecker === 'past' && scheduledEvent != null ){
-            $('.root').children().eq(i).children().eq(1).val('Event has passed')
+            $('.root').children().eq(i).children().eq(1).val('This event has passed')
         }else{
             $('.root').children().eq(i).children().eq(1).val(scheduledEvent)
         }
             
     }
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
   });
 
   
